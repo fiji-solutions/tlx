943dc0b782810c4ed98f82d72eb08637ac571921
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-05-30 21:52:11 +0200

    Print url

--- hello_world/app.py
+++ hello_world/app.py
@@ -34,7 +34,6 @@ class Granularity(Enum):
 
 def get_tlx_data(tlxCoin: str, granularity: str, granularityUnit: int, fromDate: str):
     url = "https://api.tlx.fi/functions/v1/prices/{0}?granularity={1}{2}&from={3}".format(TlxCoins[tlxCoin].value, granularityUnit, Granularity[granularity].value, fromDate)
-    print(url)
     return requests.get(url).json()
 
 
