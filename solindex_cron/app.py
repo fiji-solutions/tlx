59d15570c088a2907fcb82708cbebbcbc46ced51
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-07-26 01:06:53 +0200

    Revert "Added solindex cron"
    This reverts commit dd839182b7de91c21a7bb0261772ed865f97f656.

--- solindex_cron/app.py
+++ solindex_cron/app.py
@@ -22,24 +22,8 @@ def parse_html(html):
         cols = row.find_all('td')
         if len(cols) > 1:
             name = cols[0].text.strip()
-            price = (cols[1].text.strip()
-                     .replace('$', '')
-                     .replace('₀', '0')
-                     .replace('₁', '1')
-                     .replace('₂', '2')
-                     .replace('₃', '3')
-                     .replace('₄', '4')
-                     .replace('₅', '5')
-                     .replace('₆', '6')
-                     .replace('₇', '7')
-                     .replace('₈', '8')
-                     .replace('₉', '9')
-                     )
-            try:
-                price = float(price)
-                data.append((name, price))
-            except ValueError:
-                print(f"Skipping invalid price: {price}")
+            price = cols[1].text.strip()
+            data.append((name, price))
 
     return data
 
@@ -62,53 +46,27 @@ def store_data_in_dynamodb(data, timestamp):
                 print(f"Failed to insert {name} into DynamoDB: {e.response['Error']['Message']}")
 
 
-def store_coin_source_in_dynamodb(coin_source_data):
-    dynamodb = boto3.resource('dynamodb')
-    coin_source_table = dynamodb.Table(os.environ["table2"])
-
-    for name, source in coin_source_data:
-        try:
-            response = coin_source_table.get_item(
-                Key={
-                    'CoinName': name,
-                    'Source': source
-                }
-            )
-            if 'Item' not in response:
-                coin_source_table.put_item(
-                    Item={
-                        'CoinName': name,
-                        'Source': source
-                    }
-                )
-        except ClientError as e:
-            print(f"Failed to check/insert {name} from {source} into DynamoDB: {e.response['Error']['Message']}")
-
-
 def lambda_handler(event, context):
-    urls = {
-        "https://www.solindex.xyz/index/sol-essentials": "sol-essentials",
-        "https://www.solindex.xyz/index/memes": "memes",
-        "https://www.solindex.xyz/index/dogs": "dogs",
-        "https://www.solindex.xyz/index/cats": "cats"
-    }
+    urls = [
+        "https://www.solindex.xyz/index/sol-essentials",
+        "https://www.solindex.xyz/index/memes",
+        "https://www.solindex.xyz/index/dogs",
+        "https://www.solindex.xyz/index/cats"
+    ]
 
-    all_data = set()
-    coin_source_data = set()
+    all_data = []
     timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S+00:00')
 
-    for url, source in urls.items():
+    for url in urls:
         try:
             html = fetch_data(url)
             data = parse_html(html)
             for name, price in data:
-                all_data.add((name, price))
-                coin_source_data.add((name, source))
+                all_data.append((name, price))
         except Exception as e:
             print(f"Error fetching data from {url}: {e}")
 
-    store_data_in_dynamodb(list(all_data), timestamp)  # Convert set back to list
-    store_coin_source_in_dynamodb(list(coin_source_data))  # Convert set back to list
+    store_data_in_dynamodb(all_data, timestamp)
 
     return {
         'statusCode': 200,
