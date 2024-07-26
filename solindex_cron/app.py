859858fe4c2c6b4591160eb3f0067926369584e8
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-07-26 23:08:19 +0200

    Added cronjob

--- solindex_cron/app.py
+++ solindex_cron/app.py
@@ -62,11 +62,48 @@ def store_data_in_dynamodb(data, index_name, timestamp):
                         'Price': price,
                         'MarketCap': market_cap,
                         'Weight': weight,
-                        'UniqueId': unique_id  # Ensure uniqueness
+                        'UniqueId': unique_id
                     }
                 )
             except ClientError as e:
                 print(f"Failed to insert {name} into DynamoDB: {e.response['Error']['Message']}")
+
+
+def parse_main_page(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.select("li.flex.justify-center")
+    data = []
+
+    for item in items:
+        index_name = item.select_one("a").get("href").split("/")[-1]
+        market_cap_str = item.select_one("p.font-semibold").text.strip()
+        market_cap = parse_market_cap(market_cap_str)
+
+        data.append((index_name, market_cap))
+
+    return data
+
+
+def store_main_page_data_in_dynamodb(data, timestamp):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ["table2"])
+
+    with table.batch_writer() as batch:
+        for index_name, market_cap in data:
+            unique_id = str(uuid.uuid4())
+            composite_key = f"{index_name}#{timestamp}"
+            try:
+                batch.put_item(
+                    Item={
+                        'CompositeKey': composite_key,
+                        'IndexName': index_name,
+                        'Timestamp': timestamp,
+                        'MarketCap': market_cap,
+                        'UniqueId': unique_id
+                    }
+                )
+            except ClientError as e:
+                print(f"Failed to insert {index_name} into DynamoDB: {e.response['Error']['Message']}")
 
 
 def lambda_handler(event, context):
@@ -86,6 +123,13 @@ def lambda_handler(event, context):
             store_data_in_dynamodb(data, index_name, timestamp)
         except Exception as e:
             print(f"Error fetching data from {url}: {e}")
+
+    try:
+        main_page_html = fetch_data("https://www.solindex.xyz/")
+        main_page_data = parse_main_page(main_page_html)
+        store_main_page_data_in_dynamodb(main_page_data, timestamp)
+    except Exception as e:
+        print(f"Error fetching data from main page: {e}")
 
     return {
         'statusCode': 200,
