73fd09c45873c816645ebb4f12bb087964406628
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-07-27 21:46:24 +0200

    Added index performance

--- solindex_performance/app.py
+++ solindex_performance/app.py
@@ -1,8 +1,8 @@
 import json
 import boto3
-from datetime import datetime, timedelta
 import numpy as np
 import os
+import pandas as pd
 
 dynamodb = boto3.client('dynamodb')
 
@@ -27,6 +27,7 @@ def fetch_data_from_dynamodb(index_name, start_time, end_time):
     )
     return response['Items']
 
+
 def parse_dynamodb_data(items):
     data = {}
     for item in items:
@@ -41,6 +42,23 @@ def parse_dynamodb_data(items):
             'Price Change': float(item['PriceChange24h']['N']) if "PriceChange24h" in item else 0
         }
     return data
+
+
+def resample_data(data, granularity, granularity_unit):
+    df = pd.DataFrame(data)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df.set_index('timestamp', inplace=True)
+
+    if granularity == 'HOURS':
+        resampled_data = df.resample(f'{granularity_unit}H').mean()
+    elif granularity == 'DAYS':
+        resampled_data = df.resample(f'{granularity_unit}D').mean()
+    else:
+        resampled_data = df
+
+    resampled_data.reset_index(inplace=True)
+    return resampled_data.to_dict('records')
+
 
 # Function to calculate the weighted allocation including price change
 def calculate_allocations(data):
@@ -57,12 +75,13 @@ def calculate_allocations(data):
     allocations = {token: (weight / total_weight) * 100 for token, weight in weights.items()}
     return allocations
 
+
 # Function to simulate investing in the index and calculating performance and transactions
 def simulate_investment(data_list, initial_investment):
     portfolio_value = initial_investment
     portfolio_distribution = {}
     capital_gains = 0
-    portfolio_values = []
+    detailed_results = []
 
     for hour, timestamp in enumerate(sorted(data_list.keys())):
         data = data_list[timestamp]
@@ -89,12 +108,18 @@ def simulate_investment(data_list, initial_investment):
         portfolio_value = sum(
             portfolio_distribution[token] * data[token]["Price"] / data_list[list(data_list.keys())[0]][token]["Price"]
             for token in data)
-        portfolio_values.append(portfolio_value)
-        print(f"Portfolio value at hour {hour + 1}: ${portfolio_value:.2f}")
+
+        detailed_results.append({
+            "timestamp": timestamp,
+            "portfolio_value": portfolio_value,
+            "capital_gains": capital_gains
+        })
+
         previous_timestamp = timestamp
 
     print(f"\nTotal Capital Gains: ${capital_gains:.2f}")
-    return portfolio_values, capital_gains
+    return detailed_results
+
 
 # Additional functions for performance metrics
 def calculate_performance_metrics(portfolio_values, risk_free_rate):
@@ -115,6 +140,7 @@ def calculate_performance_metrics(portfolio_values, risk_free_rate):
             simple_omega_ratio) else None
     }
 
+
 def omega_ratio_calculation(returns, threshold=0):
     sorted_returns = np.sort(returns)
     cdf = np.arange(1, len(sorted_returns) + 1) / len(sorted_returns)
@@ -127,11 +153,13 @@ def omega_ratio_calculation(returns, threshold=0):
     omega = weighted_gains / weighted_losses if weighted_losses != 0 else np.inf
     return omega
 
+
 def simple_omega_ratio_calculation(returns, threshold=0):
     gains = returns[returns > threshold].sum() - threshold * len(returns[returns > threshold])
     losses = abs(returns[returns <= threshold].sum() - threshold * len(returns[returns <= threshold]))
     omega = gains / losses if losses != 0 else np.inf
     return omega
+
 
 def lambda_handler(event, context):
     index_name = event["queryStringParameters"]["index"]
@@ -139,6 +167,8 @@ def lambda_handler(event, context):
     end_date = event["queryStringParameters"]["toDate"]
     initial_investment = float(event["queryStringParameters"]["initialInvestment"])
     risk_free_rate = float(event["queryStringParameters"]["riskFreeRate"]) / 100
+    granularity = event["queryStringParameters"]["granularity"]
+    granularity_unit = int(event["queryStringParameters"]["granularityUnit"])
 
     # Adjust start_time and end_time
     start_time = f"{start_date} 00:00:00+00:00"
@@ -167,17 +197,14 @@ def lambda_handler(event, context):
             "body": json.dumps({"error": "No valid data to process"})
         }
 
-    # Simulate the investment
-    portfolio_values, capital_gains = simulate_investment(data_list, initial_investment)
+    # Resample data based on granularity and granularity unit
+    resampled_data = resample_data(data_list, granularity, granularity_unit)
 
-    # Ensure there are portfolio values to calculate
-    if not portfolio_values:
-        return {
-            "statusCode": 400,
-            "body": json.dumps({"error": "No portfolio values calculated"})
-        }
+    # Simulate the investment
+    detailed_results = simulate_investment(resampled_data, initial_investment)
 
     # Calculate performance metrics
+    portfolio_values = [result['portfolio_value'] for result in detailed_results]
     performance_metrics = calculate_performance_metrics(portfolio_values, risk_free_rate)
 
     return {
@@ -189,8 +216,9 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Origin": "*",
         },
         "body": json.dumps({
+            "data": detailed_results,
             "final_portfolio_value": portfolio_values[-1],
-            "total_capital_gains": capital_gains,
+            "total_capital_gains": detailed_results[-1]['capital_gains'],
             **performance_metrics
         }),
     }
