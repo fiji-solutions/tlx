2d964137273f4a3eba7798f2d2ee47effb89ba3d
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-07-27 13:57:15 +0200

    Added cronjob

--- solindex/app.py
+++ solindex/app.py
@@ -37,6 +37,11 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
+        'headers': {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Origin": "*",
+        },
         'body': json.dumps(json_data)
     }
