f4ab60487f3750cc64f2224a9c34124a0d550c19
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-07-27 14:19:00 +0200

    Added cronjob

--- solindex/app.py
+++ solindex/app.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import json
 from decimal import Decimal
+from datetime import datetime, timedelta
 
 def fetch_market_cap_data(index_name, start_date, end_date):
     dynamodb = boto3.resource('dynamodb')
@@ -21,19 +22,58 @@ def decimal_to_float(obj):
         return float(obj)
     raise TypeError
 
+def aggregate_data(data, granularity, granularity_unit):
+    aggregated_data = []
+    current_time = None
+    current_sum = 0
+    count = 0
+
+    for item in data:
+        timestamp = datetime.fromisoformat(item['Timestamp'].replace('Z', '+00:00'))
+        marketcap = decimal_to_float(item['MarketCap'])
+
+        if current_time is None:
+            current_time = timestamp
+            current_sum = marketcap
+            count = 1
+        else:
+            time_diff = timestamp - current_time
+            if granularity == 'HOURS' and time_diff < timedelta(hours=granularity_unit):
+                current_sum += marketcap
+                count += 1
+            elif granularity == 'DAYS' and time_diff < timedelta(days=granularity_unit):
+                current_sum += marketcap
+                count += 1
+            elif granularity == 'WEEKS' and time_diff < timedelta(weeks=granularity_unit):
+                current_sum += marketcap
+                count += 1
+            else:
+                aggregated_data.append({
+                    'Timestamp': current_time.isoformat(),
+                    'MarketCap': current_sum / count
+                })
+                current_time = timestamp
+                current_sum = marketcap
+                count = 1
+
+    if count > 0:
+        aggregated_data.append({
+            'Timestamp': current_time.isoformat(),
+            'MarketCap': current_sum / count
+        })
+
+    return aggregated_data
+
 def lambda_handler(event, context):
     index = event['queryStringParameters']['index']
     start_date = event['queryStringParameters']['fromDate']
     end_date = event['queryStringParameters']['toDate']
+    granularity = event['queryStringParameters'].get('granularity', 'HOURS')
+    granularity_unit = int(event['queryStringParameters'].get('granularityUnit', 1))
 
     data = fetch_market_cap_data(index, start_date, end_date)
 
-    json_data = [
-        {
-            "timestamp": item['Timestamp'],
-            "marketcap": decimal_to_float(item['MarketCap'])
-        } for item in data
-    ]
+    aggregated_data = aggregate_data(data, granularity, granularity_unit)
 
     return {
         'statusCode': 200,
@@ -43,5 +83,5 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Origin": "*",
         },
-        'body': json.dumps(json_data)
+        'body': json.dumps(aggregated_data)
     }
