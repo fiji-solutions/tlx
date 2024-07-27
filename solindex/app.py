82af2f394ab0e472458a516687d364b049222c92
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-07-27 13:38:26 +0200

    Added cronjob

--- solindex/app.py
+++ solindex/app.py
@@ -1,55 +1,113 @@
+import json
 import boto3
 import os
-import json
-from decimal import Decimal
-import csv
-import io
+import pandas as pd
+import numpy as np
 
 
-def fetch_market_cap_data(index_name):
+def fetch_market_cap_data(index_name, start_date, end_date):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ["table2"])
 
     response = table.query(
         IndexName="IndexName-Timestamp-Index",
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('IndexName').eq(index_name)
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('IndexName').eq(index_name) &
+                               boto3.dynamodb.conditions.Key('Timestamp').between(start_date, end_date)
     )
 
     items = response['Items']
     return items
 
 
-def decimal_to_float(obj):
-    if isinstance(obj, Decimal):
-        return float(obj)
-    raise TypeError
+def get_data_df(data, initial_investment):
+    df = pd.DataFrame(data)
+    df['MarketCap'] = df['MarketCap'].astype(float)
+    df['returns'] = df['MarketCap'].pct_change()
+    df['cumulative-returns'] = (1 + df['returns']).cumprod()
+    df['investment-value'] = initial_investment * df['cumulative-returns']
+    df['investment-value'].iloc[0] = initial_investment
+    df['indexed'] = df['MarketCap'] / df['MarketCap'].iloc[0] * 100
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+    df.set_index('Timestamp', inplace=True)
+    return df
 
 
-def convert_to_csv(data):
-    output = io.StringIO()
-    writer = csv.writer(output)
-    writer.writerow(["time", "open", "high", "low", "close", "volume"])  # CSV headers
+def get_volatility(df):
+    return df['returns'].std()
 
-    for item in data:
-        timestamp = item['Timestamp']
-        marketcap = decimal_to_float(item['MarketCap'])
-        writer.writerow([timestamp, marketcap, marketcap, marketcap, marketcap, 0])  # Using marketcap for OHLC and 0 for volume
 
-    return output.getvalue()
+def get_sharpe_ratio(df, risk_free_rate=0):
+    mean_return = df['returns'].mean()
+    return (mean_return - risk_free_rate) / get_volatility(df)
+
+
+def get_sortino_ratio(df, risk_free_rate=0):
+    downside_volatility = df[df['returns'] < 0]['returns'].std()
+    mean_return = df['returns'].mean()
+    return (mean_return - risk_free_rate) / downside_volatility
+
+
+def get_omega_ratio(df, threshold=0):
+    returns = df['returns'].dropna()
+    sorted_returns = np.sort(returns)
+    cdf = np.arange(1, len(sorted_returns) + 1) / len(sorted_returns)
+
+    gains = sorted_returns[sorted_returns > threshold] - threshold
+    prob_gains = 1 - cdf[np.searchsorted(sorted_returns, sorted_returns[sorted_returns > threshold]) - 1]
+
+    losses = threshold - sorted_returns[sorted_returns <= threshold]
+    prob_losses = cdf[np.searchsorted(sorted_returns, sorted_returns[sorted_returns <= threshold]) - 1]
+
+    weighted_gains = np.sum(gains * prob_gains)
+    weighted_losses = np.sum(losses * prob_losses)
+
+    omega = weighted_gains / weighted_losses if weighted_losses != 0 else np.inf
+    return omega
+
+
+def get_simple_omega_ratio(df, threshold=0):
+    returns = df['returns'].dropna()
+    gains = returns[returns > threshold].sum() - threshold * len(returns[returns > threshold])
+    losses = abs(returns[returns <= threshold].sum() - threshold * len(returns[returns <= threshold]))
+    omega = gains / losses if losses != 0 else np.inf
+    return omega
 
 
 def lambda_handler(event, context):
-    index = event['queryStringParameters']['index']
+    index = event['queryStringParameters']['coin']
+    start_date = event['queryStringParameters']['fromDate']
+    end_date = event['queryStringParameters']['toDate']
+    initial_investment = int(event['queryStringParameters']['initialInvestment'])
+    risk_free_rate = float(event['queryStringParameters']['riskFreeRate']) / 100
 
-    data = fetch_market_cap_data(index)
+    data = fetch_market_cap_data(index, start_date, end_date)
 
-    csv_data = convert_to_csv(data)
+    df = get_data_df(data, initial_investment)
+
+    volatility = get_volatility(df)
+    sharpe_ratio = get_sharpe_ratio(df, risk_free_rate)
+    sortino_ratio = get_sortino_ratio(df, risk_free_rate)
+    omega_ratio = get_omega_ratio(df)
+    simple_omega_ratio = get_simple_omega_ratio(df)
+
+    df.reset_index(inplace=True)
+    df['Timestamp'] = df['Timestamp'].astype(str)
+    df = df.replace({np.nan: None})
 
     return {
-        'statusCode': 200,
+        "statusCode": 200,
         'headers': {
-            'Content-Type': 'text/csv',
-            'Content-Disposition': 'attachment; filename=marketcap_data.csv'
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Origin": "*",
         },
-        'body': csv_data
+        "body": json.dumps({
+            "data": df.to_dict(orient='records'),
+            "volatility": volatility if not np.isnan(volatility) and not np.isinf(volatility) else None,
+            "sharpe_ratio": sharpe_ratio if not np.isnan(sharpe_ratio) and not np.isinf(sharpe_ratio) else None,
+            "sortino_ratio": sortino_ratio if not np.isnan(sortino_ratio) and not np.isinf(sortino_ratio) else None,
+            "omega_ratio": omega_ratio if not np.isnan(omega_ratio) and not np.isinf(omega_ratio) else None,
+            "simple_omega_ratio": simple_omega_ratio if not np.isnan(simple_omega_ratio) and not np.isinf(simple_omega_ratio) else None
+        }),
     }
