ac0a9fc91124f2f880a7e0933cff0d3b413631c1
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-07-28 19:49:32 +0200

    Added index performance

--- coingecko_sol_all/app.py
+++ coingecko_sol_all/app.py
@@ -28,6 +28,9 @@ def lambda_handler(event, context):
     # Fetch all records from the table
     items = fetch_all_coins(table_name)
 
+    # Order the items by the "order" field
+    items_sorted = sorted(items, key=lambda x: x['order'])
+
     # Return the items
     return {
         'statusCode': 200,
@@ -37,5 +40,5 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Origin": "*",
         },
-        'body': json.dumps(items, default=decimal_default)
+        'body': json.dumps(items_sorted, default=decimal_default)
     }
