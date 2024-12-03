5b0fe11936a3a9c670815bde060c20f489c7165b
Charalampos Moutafidis <charismoutafidis@gmail.com>
2024-12-03 14:42:59 +0100

    Added catalytics active asr

---
+++ catalytics_active_asr/app.py
@@ -0,0 +1,19 @@
+import json
+
+
+def lambda_handler():
+    return {
+        "statusCode": 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps([
+            # {
+            #     "proposalId": "",
+            #     "proposalName": "",
+            # }
+        ]),
+    }
